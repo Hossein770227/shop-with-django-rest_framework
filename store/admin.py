e4f06eef883e97e3342bfe8914c9eb8015f4346f
hossein <hossein.h.a770227@gmail.com>
2024-08-23 12:19:27 +0330

    install rest framework

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Address, Category, Product, Customer, Cart, CartItem, Discount , Order, OrderItem
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [ 'name', 'category', 'unit_price', 'inventory', 'datetime_created',]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = [ 'title','datetime_created',]
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = [ 'first_name','last_name','email', 'phone_number']
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = [ 'customer','province','city',]
